7362adb65b72c2c67f3167e9935fe02a775711dc
Sina <sina.ramsayer@uni-ulm.de>
2023-03-14 11:45:46 +0100

    Update ggwdaten.py

--- ggwdaten.py
+++ ggwdaten.py
@@ -1,7 +1,22 @@
-#Erzeugung von Gleichgewichtsdaten
+#Erzeugung von Gleichgewichtsdaten Ammoniaksynthese
 
 #Importe / Bibliotheken
 import numpy as np
 
 #
+T = 250 # K Temperatur
+R = 8,31448 # J mol^-1 K^-1 Ideale Gaskonstane
+
+#Shomate-Gleichungen
+
+
+delta_R_H_0 =
+delta_R_S_0 =
+
+#freie Standard Reaktionsenthalpie delta_R_G_0
+delta_R_G_0 = delta_R_H_0 - T * delta_R_S_0 # J mol^-1
+
+#allgemeine GGW-Konstante K_0
+K_0 = np.exp(-delta_R_G_0 / (R*T)) # 1
+
 
