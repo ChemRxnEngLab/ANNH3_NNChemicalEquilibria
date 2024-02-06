9ab93d38443ad26ad9319647b1391a66497c7712
Hannes Stagge <hannes.stagge@uni-ulm.de>
2024-02-06 11:33:41 +0100

    time onnx

---
+++ HSA_001_XXX/timing/time_onnx.py
@@ -0,0 +1,8 @@
+import onnx
+import onnxruntime as ort
+
+onnx_model = onnx.load("models/onnx/NH3_net_uniform.onnx")
+
+onnx.checker.check_model(onnx_model)
+
+ort_session = ort.InferenceSession("models/onnx/NH3_net_uniform.onnx")
