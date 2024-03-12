a7e750ac436f279ca9fae3b40ea5e3c639a3a580
Hannes Stagge <hannes.stagge@uni-ulm.de>
2024-03-12 13:48:35 +0100

    timing and speedup plots

--- HSA_001_XXX/evaluation/HSA_001_001_timing.py
+++ HSA_001_XXX/evaluation/HSA_001_001_timing.py
@@ -35,7 +35,7 @@ pos_params = {
     "ax_width": 5 * cm2inch,
 }
 
-fig = plt.figure(figsize=(7 * cm2inch, 9 * cm2inch))
+fig = plt.figure(figsize=(8 * cm2inch, 8 * cm2inch))
 ax = make_square_ax(fig, **pos_params)
 
 ax.set(
@@ -51,9 +51,46 @@ ax.plot(ns_NN_1e2, ts_NN_1e2, "--o", label="aNN (gpu, 1e2 samples)")
 ax.plot(ns_NN_1e3, ts_NN_1e3, "--o", label="aNN (gpu, 1e3 samples)")
 # ax.plot(ns_NN_1e4, ts_NN_1e4, "--o", label="aNN (cpu)")
 # ax.plot(ns_NN_1e5, ts_NN_1e5, "--o", label="aNN (cpu)")
-plt.legend(frameon=True, bbox_to_anchor=(0.4, 1), loc="lower center")
+# plt.legend(frameon=True, bbox_to_anchor=(0.4, 1), loc="lower center")
 
 plt.tight_layout()
 plt.savefig(Path.cwd() / "figures" / "HSA_001_001_timing.png", dpi=300)
+plt.savefig(Path.cwd() / "figures" / "HSA_001_001_timing.svg")
 
+plt.show()
+
+lambda_NN_cpu = ts[1:] / ts_NN_cpu
+lambda_NN_1e2 = ts[1:] / ts_NN_1e2
+lambda_NN_1e3 = ts[1:] / ts_NN_1e3
+lambda_NN_1e4 = ts[1:] / ts_NN_1e4
+lambda_NN_1e5 = ts[1:] / ts_NN_1e5
+
+fig = plt.figure(figsize=(7 * cm2inch, 9 * cm2inch))
+ax = make_square_ax(fig, **pos_params)
+
+ax.set(
+    xlabel="n",
+    ylabel="$\Phi$ / 1",
+    xscale="log",
+    yscale="log",
+)
+
+ax.plot(
+    ns_NN_cpu,
+    lambda_NN_cpu,
+    "o",
+    label="aNN (cpu)",
+)
+ax.plot(ns_NN_cpu, lambda_NN_cpu, "--o", label="aNN (cpu)")
+ax.plot(ns_NN_1e2, lambda_NN_1e2, "--o", label="aNN (gpu, 1e2 samples)")
+ax.plot(ns_NN_1e3, lambda_NN_1e3, "--o", label="aNN (gpu, 1e3 samples)")
+# ax.plot(ns_NN_1e4, lambda_NN_1e4, "--o", label="aNN (cpu)")
+# ax.plot(ns_NN_1e5, lambda_NN_1e5, "--o", label="aNN (cpu)")
+
+ax.set_ylim(1e-1, 1e2)
+
+plt.legend(frameon=True, loc="lower right")
+plt.tight_layout()
+plt.savefig(Path.cwd() / "figures" / "HSA_001_001_speedup.png", dpi=300)
+plt.savefig(Path.cwd() / "figures" / "HSA_001_001_speedup.svg")
 plt.show()
